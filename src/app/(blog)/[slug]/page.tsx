d9e20b5a061943cf4811271feea61ff44d3fc406
quanghoang02 <quang.hoang@aiaracorp.com>
2023-03-18 18:04:22 +0700

    haha

--- src/app/(blog)/[slug]/page.tsx
+++ src/app/(blog)/[slug]/page.tsx
@@ -1,26 +1,32 @@
+
+
 import LargeHeading from "@/components/ui/LargeHeading";
 import Paragraph from "@/components/ui/Paragraph";
 import { primaryFormatDate } from "../../../utils/formatDate";
 import { getPost } from "@/utils/post";
 import readingTime from "reading-time";
 import { notFound } from "next/navigation";
-import Markdown from "markdown-to-jsx";
 import { Metadata } from "next";
-
+import ReactMarkdown from "react-markdown";
+import Code from "@/ui/Code";
+import SimpleBar from 'simplebar-react'
 interface IBlogDetailPageProps {
   params: {
     slug: string;
   };
 }
+import {type Language } from 'prism-react-renderer'
 
-export async function generateMetadata({ params }: IBlogDetailPageProps): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: IBlogDetailPageProps): Promise<Metadata> {
   const { slug } = params;
   const post = await getPost(slug);
   if (!post) notFound();
-  return { 
+  return {
     title: post.title,
     description: post.description,
-   }
+  };
 }
 
 export default async function BlogDetailPage({ params }: IBlogDetailPageProps) {
@@ -44,9 +50,32 @@ export default async function BlogDetailPage({ params }: IBlogDetailPageProps) {
                   __html: post.htmlContent as string,
                 }}
               /> */}
-              <div className='w-full prose dark:prose-invert prose-slate '>
-              <Markdown>{post.mdContent || ''}</Markdown>
-              </div>
+            <div className="w-full prose dark:prose-invert prose-slate ">
+              {/* <Markdown>{post.mdContent || ''}</Markdown> */}
+              <ReactMarkdown
+                components={{
+                  code({ node, inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || "");
+                    return !inline && match ? (
+                      <div className="p-6 mt-2 border rounded-md border-slate-300 bg-slate-100 dark:border-slate-700 dark:bg-slate-900">
+                      <Code
+                        animated
+                        language={match[1] as Language}
+                        show
+                        code={String(children).replace(/\n$/, "")}
+                      />
+                      </div>
+                    ) : (
+                      <code className={className} {...props}>
+                        {children}
+                      </code>
+                    );
+                  },
+                }}
+              >
+                {post.mdContent || ""}
+              </ReactMarkdown>
+            </div>
           </div>
         </div>
       </div>
